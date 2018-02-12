a1961372bc5b6da8659a91072396961df8f1af08
Maximilian Bayerl <maximilian.bayerl@contactimpact.de>
2018-02-12 17:17:10 +0100

    angular-mapbox circle-layer km zoom fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
-
+import { MapComponent } from './map/map.component';
+import { PicComponent } from './pic/pic.component';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MapComponent,
+    PicComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    FormsModule,
+    NgxMapboxGLModule.forRoot({
+      accessToken: 'pk.eyJ1IjoibWF4aW1pbGlhbmJheWVybCIsImEiOiJjajk1bXh1bWwwMGhwMnFwYXFsMzUxNGJuIn0.vGPBZA8AcBsE9aaWrKZ_Yg' // accessToken can also be set per map (accessToken input of mgl-map)
+    }),
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
